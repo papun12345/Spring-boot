a91c755606669d937771370e37d6a4ac634a22c3
papun12345 <50276904+papun12345@users.noreply.github.com>
2019-07-09 18:40:33 +0530

    Update SpringBootHelloWorldApplication.java

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -2,16 +2,12 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
  
 @SpringBootApplication
-public class SpringBootHelloWorldApplication extends SpringBootServletInitializer {
+public class SpringBootHelloWorldApplication {
 	
-	@Override
- 	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
-         return app.sources(SpringBootWarExampleApplication.class);
-     }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
